a8f2398fc976601824048dc99c031d07bfaf000d
Ramez Taher <ramezgamer11@gmail.com>
2023-04-03 23:59:55 +0100

    fix cors issues

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ const MONGODB_URI = process.env.MONGODB_URI
 const PORT = process.env.PORT
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  )
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+  res.setHeader("Access-Control-Allow-Credentials", true)
   next()
 })
 
